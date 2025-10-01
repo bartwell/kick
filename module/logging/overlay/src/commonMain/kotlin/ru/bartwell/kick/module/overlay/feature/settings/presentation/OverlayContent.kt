15e57565a38058329b7c06cd8cd50d9ca187ddc6
Бажанов Артем <web@bartwell.ru>
2025-10-01 23:04:06 +0300

    Fix logging-stub

--- module/logging/overlay/src/commonMain/kotlin/ru/bartwell/kick/module/overlay/feature/settings/presentation/OverlayContent.kt
+++ module/logging/overlay/src/commonMain/kotlin/ru/bartwell/kick/module/overlay/feature/settings/presentation/OverlayContent.kt
@@ -9,9 +9,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
