30f3237d28975cea42c5b6b420c9e47d1a9f85d0
Бажанов Артем <web@bartwell.ru>
2026-02-07 12:07:56 +0300

    Add ObjectRunnerRenderer

--- module/logging/runner/src/commonMain/kotlin/ru/bartwell/kick/module/runner/core/renderer/ImageRunnerRenderer.kt
+++ module/logging/runner/src/commonMain/kotlin/ru/bartwell/kick/module/runner/core/renderer/ImageRunnerRenderer.kt
@@ -8,14 +8,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ru.bartwell.kick.module.runner.core.data.PlatformImage
 import ru.bartwell.kick.module.runner.core.data.Content
-import ru.bartwell.kick.module.runner.core.data.fromNative
+import ru.bartwell.kick.module.runner.core.data.RunnerRenderer
 
 /**
  * Renderer that shows a platform image (Bitmap/UIImage/BufferedImage/ImageBitmap).
